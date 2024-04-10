267060fbda06516a14835b92209cb9d8fbc0ef05
Your Name <you@example.com>
2024-04-10 10:13:22 +0200

    PID

--- memcached_client/config.h
+++ memcached_client/config.h
@@ -3,6 +3,7 @@
 
 #ifndef CONFIG_H
 #define CONFIG_H
+
 #include <stdint.h>
 #include "generate.h"
 #include "pthread.h"
@@ -17,55 +18,57 @@
 #include "magic2_breakpoint.h"
 #endif
 
-typedef enum{
-  SCALED_TWITTER, PURE_ZIPFIAN
+typedef enum {
+    SCALED_TWITTER, PURE_ZIPFIAN
 } DISTRIBUTION;
 
 struct config {
-  int protocol_mode;
+    int protocol_mode;
 
-  int n_cpus;
-  int n_keys;
+    int n_cpus;
+    int n_keys;
 
-  int n_workers;
-  int n_servers;
-  struct worker** workers;
-  int n_connections_total;
+    int n_workers;
+    int n_servers;
+    struct worker **workers;
+    int n_connections_total;
 
-  int run_time;
-  int stats_time;
-  int naggles;
-  int multiget_size;
-  char* server_ip_address[MAX_SERVERS];
-  char* server_file;
-  char* input_file;
-  char* output_file;
-  int server_port[MAX_SERVERS];
-  int server_memory;
-  int keysToPreload;
-  int scaling_factor;
-  float get_frac;
-  float multiget_frac;
-  float incr_frac;
-  struct key_list* key_list;
-  struct int_dist* key_pop_dist;
-  struct int_dist* value_size_dist;
-  struct int_dist* multiget_dist;
-  struct int_dist* interarrival_dist;
-  struct dep_dist* dep_dist;
-  int arrival_distribution_type;
-  int received_warmup_keys;
-  int rps;
-  int fixed_size;
-  int zynga;
-  int random_seed;
-  int pre_load;
-  int bad_multiget;
+    int run_time;
+    int stats_time;
+    int naggles;
+    int multiget_size;
+    char *server_ip_address[MAX_SERVERS];
+    char *server_file;
+    char *input_file;
+    char *output_file;
+    int server_port[MAX_SERVERS];
+    int server_memory;
+    int keysToPreload;
+    int scaling_factor;
+    float get_frac;
+    float multiget_frac;
+    float incr_frac;
+    struct key_list *key_list;
+    struct int_dist *key_pop_dist;
+    struct int_dist *value_size_dist;
+    struct int_dist *multiget_dist;
+    struct int_dist *interarrival_dist;
+    struct dep_dist *dep_dist;
+    int arrival_distribution_type;
+    int received_warmup_keys;
+    int rps;
+    int fixed_size;
+    int zynga;
+    int random_seed;
+    int pre_load;
+    int bad_multiget;
 
-  uint32_t current_request_uid;
-  double ALPHA;
-  DISTRIBUTION distribution;
-  int randomValue;
+    uint32_t current_request_uid;
+    double ALPHA;
+    DISTRIBUTION distribution;
+    int randomValue;
+
+    float SLO;
 };
 
 #endif
--- memcached_client/stats.c
+++ memcached_client/stats.c
@@ -8,143 +8,175 @@
 #include "loader.h"
 #include <assert.h>
 #include "worker.h"
+#include "PID.h"
 
 pthread_mutex_t stats_lock = PTHREAD_MUTEX_INITIALIZER;
 struct timeval start_time;
 struct memcached_stats global_stats;
+PIDController pid;
 
-void addSample(struct stat* stat, float value) {
-  stat->s0 += 1.0;
-  stat->s1 += value;
-  stat->s2 += value*value;
-  stat->min = fmin(stat->min,value);
-  stat->max = fmax(stat->max,value);
+void addSample(struct stat *stat, float value) {
+    stat->s0 += 1.0;
+    stat->s1 += value;
+    stat->s2 += value * value;
+    stat->min = fmin(stat->min, value);
+    stat->max = fmax(stat->max, value);
 
-  if(value < .001){
-    int bin = (int)(value*10000000);
-    stat->micros[bin] += 1;
-  } else if( value < 5.0){
-    int bin = value * 10000.0;
-    assert(bin < 50001);
-    stat->millis[bin] += 1;
-  } else if (value < 999){
-    int bin = (int)value;
-    stat->fulls[bin] += 1;
-  } else {
-    int bin = (int)value/1000;
-    if (bin > 999){
-      bin = 999;
+    if (value < .001) {
+        int bin = (int) (value * 10000000);
+        stat->micros[bin] += 1;
+    } else if (value < 5.0) {
+        int bin = value * 10000.0;
+        assert(bin < 50001);
+        stat->millis[bin] += 1;
+    } else if (value < 999) {
+        int bin = (int) value;
+        stat->fulls[bin] += 1;
+    } else {
+        int bin = (int) value / 1000;
+        if (bin > 999) {
+            bin = 999;
+        }
+        stat->fulls[bin] += 1;
     }
-    stat->fulls[bin] += 1;
-  }
 
 
 }//End addAvgSample()
 
-double getAvg(struct stat* stat) {
-  return (stat->s1/stat->s0);
+double getAvg(struct stat *stat) {
+    return (stat->s1 / stat->s0);
 }//End getAvg()
 
-double getStdDev(struct stat* stat) {
-  return sqrt((stat->s0*stat->s2 - stat->s1*stat->s1)/(stat->s0*(stat->s0 - 1)));
+double getStdDev(struct stat *stat) {
+    return sqrt((stat->s0 * stat->s2 - stat->s1 * stat->s1) / (stat->s0 * (stat->s0 - 1)));
 }//End getStdDev()
 
 //Should we exit because time has expired?
-void checkExit(struct config* config) {
+void checkExit(struct config *config) {
 
-  int runTime = config->run_time;
-  struct timeval currentTime;
-  gettimeofday(&currentTime, NULL);
-  double totalTime = currentTime.tv_sec - start_time.tv_sec + 1e-6*(currentTime.tv_sec - start_time.tv_sec);
-  if(totalTime >= runTime && runTime >0) {
-    printf("Ran for %f, exiting\n", totalTime);
-    exit(0);
-  }
+    int runTime = config->run_time;
+    struct timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+    double totalTime = currentTime.tv_sec - start_time.tv_sec + 1e-6 * (currentTime.tv_sec - start_time.tv_sec);
+    if (totalTime >= runTime && runTime > 0) {
+        printf("Ran for %f, exiting\n", totalTime);
+        exit(0);
+    }
 
 }//End checkExit()
 
-double findQuantile(struct stat* stat, double quantile) { 
+double findQuantile(struct stat *stat, double quantile) {
 
-  //Find the 95th-percentile
-  int nTillQuantile = global_stats.response_time.s0 * quantile;
-  int  count = 0;
-  int i;
-  for( i = 0; i < 10000; i++) {
-    count += stat->micros[i];
-    if( count >= nTillQuantile ){
-      double quantile = (i+1) * .0000001;
-      return quantile;
-    }
-  }//End for i
+    //Find the 95th-percentile
+    int nTillQuantile = global_stats.response_time.s0 * quantile;
+    int count = 0;
+    int i;
+    for (i = 0; i < 10000; i++) {
+        count += stat->micros[i];
+        if (count >= nTillQuantile) {
+            double quantile = (i + 1) * .0000001;
+            return quantile;
+        }
+    }//End for i
 
-  for( i = 0; i < 50000; i++) {
-    count += stat->millis[i];
-    if( count >= nTillQuantile ){
-      double quantile = (i+1) * .0001;
-      return quantile;
-    }
-  }//End for i
-  printf("count  %d\n", count);
+    for (i = 0; i < 50000; i++) {
+        count += stat->millis[i];
+        if (count >= nTillQuantile) {
+            double quantile = (i + 1) * .0001;
+            return quantile;
+        }
+    }//End for i
+    printf("count  %d\n", count);
 
-  for( i = 0; i < 1000; i++) {
-    count += stat->fulls[i];
-    if( count >= nTillQuantile ){
-      double quantile = i+1;
-      return quantile;
-    }
-  }//End for i
-  return 1000;
+    for (i = 0; i < 1000; i++) {
+        count += stat->fulls[i];
+        if (count >= nTillQuantile) {
+            double quantile = i + 1;
+            return quantile;
+        }
+    }//End for i
+    return 1000;
 
 }//End findQuantile()
 
-void printGlobalStats(struct config* config) {
+void printGlobalStats(struct config *config) {
 
-  pthread_mutex_lock(&stats_lock);
-  struct timeval currentTime;
-  gettimeofday(&currentTime, NULL);
-  double timeDiff = currentTime.tv_sec - global_stats.last_time.tv_sec + 1e-6*(currentTime.tv_sec - global_stats.last_time.tv_sec);
-  double rps = global_stats.requests/timeDiff;
-  double std = getStdDev(&global_stats.response_time);
-  double q90 = findQuantile(&global_stats.response_time, .90);
-  double q95 = findQuantile(&global_stats.response_time, .95);
-  double q99 = findQuantile(&global_stats.response_time, .99);
+    pthread_mutex_lock(&stats_lock);
+    struct timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+    double timeDiff = currentTime.tv_sec - global_stats.last_time.tv_sec +
+                      1e-6 * (currentTime.tv_sec - global_stats.last_time.tv_sec);
+    double rps = global_stats.requests / timeDiff;
+    double std = getStdDev(&global_stats.response_time);
+    double q90 = findQuantile(&global_stats.response_time, .90);
+    double q95 = findQuantile(&global_stats.response_time, .95);
+    double q99 = findQuantile(&global_stats.response_time, .99);
 
-  printf("%10s,%10s,%8s,%16s, %8s,%11s,%10s,%13s,%10s,%10s,%10s,%12s,%10s,%10s,%11s,%14s\n", "unix_ts", "timeDiff", "rps", "requests", "gets", "sets",  "hits", "misses", "avg_lat", "90th", "95th", "99th", "std", "min", "max", "avgGetSize");
-  printf("%10ld, %10f, %9.1f,  %10d, %10d, %10d, %10d, %10d, %10f, %10f, %10f, %10f, %10f, %10f, %10f, %10f\n", 
-		currentTime.tv_sec, timeDiff, rps, global_stats.requests, global_stats.gets, global_stats.sets, global_stats.hits, global_stats.misses,
-		1000*getAvg(&global_stats.response_time), 1000*q90, 1000*q95, 1000*q99, 1000*std, 1000*global_stats.response_time.min, 1000*global_stats.response_time.max, getAvg(&global_stats.get_size));
-  int i;
-  printf("Outstanding requests per worker:\n");
-  for(i=0; i<config->n_workers; i++){
-    printf("%d ", config->workers[i]->n_requests);
-  } 
-  printf("\n");
-  //Reset stats
-  memset(&global_stats, 0, sizeof(struct memcached_stats));
-  global_stats.response_time.min = 1000000;
-  global_stats.last_time = currentTime;
+    printf("%10s,%10s,%8s,%16s, %8s,%11s,%10s,%13s,%10s,%10s,%10s,%12s,%10s,%10s,%11s,%14s\n", "unix_ts", "timeDiff",
+           "rps", "requests", "gets", "sets", "hits", "misses", "avg_lat", "90th", "95th", "99th", "std", "min", "max",
+           "avgGetSize");
+    printf("%10ld, %10f, %9.1f,  %10d, %10d, %10d, %10d, %10d, %10f, %10f, %10f, %10f, %10f, %10f, %10f, %10f\n",
+           currentTime.tv_sec, timeDiff, rps, global_stats.requests, global_stats.gets, global_stats.sets,
+           global_stats.hits, global_stats.misses,
+           1000 * getAvg(&global_stats.response_time), 1000 * q90, 1000 * q95, 1000 * q99, 1000 * std,
+           1000 * global_stats.response_time.min, 1000 * global_stats.response_time.max,
+           getAvg(&global_stats.get_size));
+    int i;
+    printf("Outstanding requests per worker:\n");
+    for (i = 0; i < config->n_workers; i++) {
+        printf("%d ", config->workers[i]->n_requests);
+    }
+    printf("\n");
 
-  checkExit(config);
-  pthread_mutex_unlock(&stats_lock);
+
+    // TODO change measurement (percentile) based on config
+    // TODO set SLO in config using command args
+
+    if (config->SLO != -1) {
+        // Update PID struct
+        PIDController_Update(&pid, config->SLO, q90);
+        printf("Setting new RPS to: %f\n", pid.out);
+
+        // Update worker distribution
+        int meanInterarrival = 1.0 / (((float) pid.out) / (float) config->n_workers) * 1e6;
+        for (i = 0; i < config->n_workers; i++) {
+            config->workers[i]->config->interarrival_dist = createExponentialDistribution(meanInterarrival);
+        }
+    }
+
+    //Reset stats
+    memset(&global_stats, 0, sizeof(struct memcached_stats));
+    global_stats.response_time.min = 1000000;
+    global_stats.last_time = currentTime;
+
+    checkExit(config);
+    pthread_mutex_unlock(&stats_lock);
 
 }//End printGlobalStats()
 
 
 //Print out statistics every second
-void statsLoop(struct config* config) {
+void statsLoop(struct config *config) {
 
-  pthread_mutex_lock(&stats_lock);
-  gettimeofday(&start_time, NULL);
-  pthread_mutex_unlock(&stats_lock);
+    pthread_mutex_lock(&stats_lock);
+    gettimeofday(&start_time, NULL);
+    pthread_mutex_unlock(&stats_lock);
 
-  sleep(2);
-  printf("Stats:\n");
-  printf("-------------------------\n");
-  while(1) {
-    printGlobalStats(config);
-    sleep(config->stats_time);
-  }//End while()
+    // TODO tune controller parameters
+    /* Controller parameters */
+    float PID_KP = 2.0f;
+    float PID_KI = 0.5f;
+    float PID_KD = 0.25f;
+    float PID_TAU = 0.02f;
+    pid = (PIDController) {PID_KP, PID_KI, PID_KD, PID_TAU, config->stats_time};
 
+    sleep(2);
+    printf("Stats:\n");
+    printf("-------------------------\n");
+    while (1) {
+        printGlobalStats(config);
+        sleep(config->stats_time);
+    }//End while()
 
 }//End statisticsLoop()
 
